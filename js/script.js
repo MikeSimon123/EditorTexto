9049c2304aad14db87a303c94355f7206262ada4
MikeSimon123 <mikesimonmikesimon@gmail.com>
2025-11-09 00:36:23 -0300

    versão com projetos

--- js/script.js
+++ js/script.js
@@ -1,17 +1,49 @@
-const formEditor = document.getElementById("editor-form")
-const editorArea = document.getElementById("editor-area")
-const nomeArquivo = document.getElementById("editor-name")
-const extensao = document.getElementById("editor-extensao")
+const projetos = document.getElementById("projetosArea")
+const addProjectButton = document.getElementById("addProjectButton")
+const overlay = document.getElementById("overlay")
+const formAddProject = document.getElementById("projetoDesc")
+const cancelProject = document.getElementById("cancelProject")
+const createProject = document.getElementById("createProject")
+const projectName = document.getElementById("projectName")
 
-formEditor.addEventListener("submit", function(e){
-    e.preventDefault()
-    //console.log(editorArea.value)
-    let conteudo = editorArea.value
-    let objArquivo = new Blob([conteudo], {type:"text/plain"})
-    let link = document.createElement("a")
-    link.href = URL.createObjectURL(objArquivo)
-    link.download = `${nomeArquivo.value}.${extensao.value}` || `arquivo.${extensao.value}`
-    link.click()
+window.addEventListener("DOMContentLoaded", function(){
+    if(localStorage.hasOwnProperty("projetos")){
+        JSON.parse(localStorage.getItem("projetos")).forEach(elemento => {
+            let projeto = document.createElement("div")
+            projeto.innerHTML = `
+                <p>${elemento.name}</p>
+                <img src='img/close.png' class='removeButton' id='removeButton${elemento.name}'>
+            `
+            projeto.classList.add("projeto")
+            projetos.appendChild(projeto)
+        })
+    } else {
+        projetos.innerHTML = "<p id='no-projects'>Ainda não existem projetos salvos</p>"
+    }
+})
 
-    URL.revokeObjectURL(link.href)
+cancelProject.addEventListener("click", function(){
+    overlay.style.display = "none"
+    formAddProject.style.display = "none"
+})
+
+createProject.addEventListener("click", function(){
+    if(localStorage.hasOwnProperty("projetos")){
+        let projeto = {name:projectName.value, arquivos:[]}
+        let statusAtualizado = JSON.parse(localStorage.getItem("projetos"))
+        statusAtualizado.push(projeto)
+        localStorage.setItem("projetos", JSON.stringify(statusAtualizado))
+    }else {
+        let projeto = {name:projectName.value, arquivos:[]}
+        let statusAtualizado = []
+        statusAtualizado.push(projeto)
+        localStorage.setItem("projetos", JSON.stringify(statusAtualizado))
+    }
+})
+
+addProjectButton.addEventListener("click", function(){
+    overlay.style.display = "block"
+    formAddProject.style.display = "flex"
+    formAddProject.style.top = `${(document.body.offsetHeight - formAddProject.offsetHeight)/2}px`
+    formAddProject.style.left = `${(document.body.offsetWidth - formAddProject.offsetWidth)/2}px`
 })
