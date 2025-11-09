b80fc88438104976309797535a390b982ba84f70
MikeSimon123 <mikesimonmikesimon@gmail.com>
2025-11-09 10:09:35 -0300

    Adicionando config projetos

--- js/script.js
+++ js/script.js
@@ -6,18 +6,41 @@ const cancelProject = document.getElementById("cancelProject")
 const createProject = document.getElementById("createProject")
 const projectName = document.getElementById("projectName")
 
-window.addEventListener("DOMContentLoaded", function(){
-    if(localStorage.hasOwnProperty("projetos")){
-        JSON.parse(localStorage.getItem("projetos")).forEach(elemento => {
-            let projeto = document.createElement("div")
-            projeto.innerHTML = `
-                <p>${elemento.name}</p>
-                <img src='img/close.png' class='removeButton' id='removeButton${elemento.name}'>
-            `
-            projeto.classList.add("projeto")
-            projetos.appendChild(projeto)
+function atualizarProjetos() {
+    projetos.innerHTML = ""
+    JSON.parse(localStorage.getItem("projetos")).forEach(elemento => {
+        
+        let projeto = document.createElement("div")
+        projeto.innerHTML = `
+            <p>${elemento.name}</p>
+            <img src='img/close.png' class='removeButton' id='removeButton${elemento.name}'>
+        `
+        projeto.classList.add("projeto")
+        projetos.appendChild(projeto)
+        let indexA = 0
+        document.getElementById(`removeButton${elemento.name}`).addEventListener("click", function(){
+            JSON.parse(localStorage.getItem("projetos")).forEach((elementoJSON, index) => {
+                if(elementoJSON.name == elemento.name){
+                    indexA = index
+                }
+            })
+            let desatualizado = JSON.parse(localStorage.getItem("projetos"))
+            desatualizado.splice(indexA, 1)
+            localStorage.setItem("projetos", JSON.stringify(desatualizado))
+            atualizarProjetos()
+            if(JSON.parse(localStorage.getItem("projetos")).length <= 0){
+                projetos.style.display = "flex"
+            projetos.innerHTML = "<p id='no-projects'>Ainda não existem projetos salvos</p>"
+            }
         })
+    })
+}
+window.addEventListener("DOMContentLoaded", function(){
+    if(localStorage.hasOwnProperty("projetos") && JSON.parse(localStorage.getItem("projetos")).length > 0){
+        projetos.style.display = "grid"
+        atualizarProjetos()
     } else {
+        projetos.style.display = "flex"
         projetos.innerHTML = "<p id='no-projects'>Ainda não existem projetos salvos</p>"
     }
 })
